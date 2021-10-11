83bf4ac0ac5f12f57dfb9dd6cbc6a8c33880d103
Senishine <oksastar89@mail.ru>
2021-10-12 01:03:27 +0300

    lesson 2

---
+++ geekshop/geekshop/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def main(request):
+    title = 'Магазин'
+    context = {
+        'title': title,
+    }
+    return render(request, 'geekshop/index.html', context)
+
+
+def contacts(request):
+    title = 'Контакты'
+    context = {
+        'title': title,
+    }
+    return render(request, 'geekshop/contact.html', context)
